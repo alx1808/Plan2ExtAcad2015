f0f4924186f25446d04dd4ff364f4f62b9b9355b
Alexander Ausweger <alexander.ausweger@aon.at>
2017-07-19 08:46:35 +0200

    cdff

--- Plan2Ext/Globs.cs
+++ Plan2Ext/Globs.cs
@@ -48,6 +48,7 @@ namespace Plan2Ext
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Convert.ToString((typeof(Globs))));
         static Globs()
         {
+
             if (log4net.LogManager.GetRepository(System.Reflection.Assembly.GetExecutingAssembly()).Configured == false)
             {
                 log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(System.IO.Path.Combine(new System.IO.FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).DirectoryName, "_log4net.config")));
