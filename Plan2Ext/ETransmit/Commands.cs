80285a49303505310e63796cc6e8905dca54e26a
Alexander Ausweger <alexander.ausweger@aon.at>
2019-04-09 08:28:26 +0200

    Plan2ETransmit

---
+++ Plan2Ext/ETransmit/Commands.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+// ReSharper disable StringLiteralTypo
+
+namespace Plan2Ext.ETransmit
+{
+    // ReSharper disable once UnusedMember.Global
+    public class Commands
+    {
+        [CommandMethod("Plan2ETransmit")]
+        // ReSharper disable once UnusedMember.Global
+        public void Plan2ETransmit()
+        {
+            try
+            {
+                CheckXRefBinding(false);
+            }
+            catch (System.Exception ex)
+            {
+                Application.ShowAlertDialog(string.Format(CultureInfo.CurrentCulture,
+                    "Fehler in Plan2ETransmit aufgetreten! {0}", ex.Message));
+            }
+        }
+
+        private void CheckXRefBinding(bool insertBind)
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var xrefObjectIds = Globs.GetAllMsXrefIds(doc.Database);
+            using (ObjectIdCollection acXrefIdCol = new ObjectIdCollection())
+            {
+                foreach (var xrefObjectId in xrefObjectIds)
+                {
+                    acXrefIdCol.Add(xrefObjectId);
+                    
+                }
+                if (acXrefIdCol.Count > 0)
+                    doc.Database.BindXrefs(acXrefIdCol, insertBind);
+            }
+        }
+    }
+}
