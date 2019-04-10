e39a19eeb5e51c8bd5193d20fd39653e7cae3705
Alexander Ausweger <alexander.ausweger@aon.at>
2019-04-10 12:27:38 +0200

    Plan2ETransmit: GetUserInputForInsertBind, GetMultipleFileNames

--- Plan2Ext/ETransmit/Commands.cs
+++ Plan2Ext/ETransmit/Commands.cs
@@ -1,6 +1,8 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using Autodesk.AutoCAD.ApplicationServices.Core;
 using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
 // ReSharper disable StringLiteralTypo
 
@@ -15,13 +17,50 @@ namespace Plan2Ext.ETransmit
         {
             try
             {
-                CheckXRefBinding(false);
+                var dirName = string.Empty;
+                string[] dwgFileNames = null;
+                if (!Globs.GetMultipleFileNames(
+                    "AutoCAD-Zeichnung",
+                    "Dwg", 
+                    "Verzeichnis mit Zeichnungen für ETransmit",
+                    "Zeichnungen für ETransmit", 
+                    ref dwgFileNames, 
+                    ref dirName, 
+                    Application.GetSystemVariable("DWGPREFIX").ToString()))
+                {
+                    return;
+                }
+
+                //var insertBind = GetUserInputForInsertBind();
+                //CheckXRefBinding(insertBind);
+            }
+            catch (OperationCanceledException)
+            {
+                // cancelled by user
             }
             catch (System.Exception ex)
             {
                 Application.ShowAlertDialog(string.Format(CultureInfo.CurrentCulture,
                     "Fehler in Plan2ETransmit aufgetreten! {0}", ex.Message));
             }
+        }
+
+        private bool GetUserInputForInsertBind()
+        {
+            var pKeyOpts = new PromptKeywordOptions("") { Message = "\nXRefs Binden/<Einfügen>: " };
+            pKeyOpts.Keywords.Add("Binden");
+            pKeyOpts.Keywords.Add("Einfügen");
+            pKeyOpts.AllowNone = true;
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
+            PromptResult pKeyRes = ed.GetKeywords(pKeyOpts);
+            if (pKeyRes.Status == PromptStatus.Cancel)
+            {
+                throw new OperationCanceledException();
+            }
+            if (pKeyRes.Status == PromptStatus.None || pKeyRes.StringResult == "Einfügen") return true;
+            if (pKeyRes.Status == PromptStatus.OK) return false;
+            throw new InvalidOperationException("Userinput Status: " + pKeyRes.Status);
         }
 
         private void CheckXRefBinding(bool insertBind)
@@ -33,7 +72,7 @@ namespace Plan2Ext.ETransmit
                 foreach (var xrefObjectId in xrefObjectIds)
                 {
                     acXrefIdCol.Add(xrefObjectId);
-                    
+
                 }
                 if (acXrefIdCol.Count > 0)
                     doc.Database.BindXrefs(acXrefIdCol, insertBind);
