796eed576be4e144351c0a1ed76be69f577f2a07
Alexander Ausweger <alexander.ausweger@aon.at>
2019-05-24 06:36:00 +0200

    Plan2GenerateOeffBoundaries: EditorHelper Refactoring

--- Plan2Ext/EditorHelper.cs
+++ Plan2Ext/EditorHelper.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.AutoCAD.ApplicationServices.Core;
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 // ReSharper disable IdentifierTypo
 
 namespace Plan2Ext
 {
-    internal class EditorHelper
+    internal static class EditorHelper
     {
         public static ObjectId Entlast()
         {
